d1b7d18c480ae6b02c5224c6f760ede29bbeb493
Adam Bergman <bergman.adamt@gmail.com>
2020-02-21 20:29:25 -0500

    Initial tool

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,196 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      Duration of image display (seconds): <input type="number" v-model.number="duration" />
+    </div>
+    <div>
+      Cross fade duration (seconds): <input type="number" v-model.number="crossfade" />
+    </div>
+    <div>
+      Total Images: <input type="number" v-model.number="totalImages" />
+    </div>
+    <div>
+      <h2>PREVIEW</h2>
+      <div class="preview" v-html="htmlString" />
+      <v-style>{{ cssString }}</v-style>
+    </div>
+    <div>
+      <h2>HTML</h2>
+      <highlight-code lang="xml">
+        {{ htmlString }}
+      </highlight-code>
+    </div>
+    <div>
+      <h2>SCSS</h2>
+      <highlight-code lang="scss">
+        {{ scssString }}
+      </highlight-code>
+    </div>
+    <div>
+      <h2>CSS</h2>
+      <highlight-code lang="css">
+        {{ cssString }}
+      </highlight-code>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+// For "n" images You must define:
+// a=presentation time for one image
+// b=duration for cross fading
+// Total animation-duration is of course t=(a+b)*n
+// animation-delay = t/n or = a+b
+// Percentage for keyframes:
+// 0%
+// a/t*100%
+// (a+b)/t*100% = 1/n*100%
+// 100%-(b/t*100%)
+// 100%
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      duration: 5, // presentation time for one image (a)
+      crossfade: 2, // duration for cross fading (b)
+      totalImages: 2, // total number of images (n)
+    };
+  },
+  computed: {
+    // total animation-duration (t) = (a + b) * n
+    totalDuration() {
+      return (this.duration + this.crossfade) * this.totalImages;
+    },
+    // animation delay t / n OR a + b
+    animationDelay() {
+      return this.duration + this.crossfade;
+    },
+    // a / t * 100%
+    percentFirst() {
+      const calculation = this.duration / this.totalDuration;
+      return Math.round(calculation * 100);
+    },
+    // (a + b) / t * 100% OR 1 / n * 100%
+    percentMiddle() {
+      const calculation = (this.duration + this.crossfade) / this.totalDuration;
+      return Math.round(calculation * 100);
+    },
+    // 100% - (b / t * 100%)
+    percentLast() {
+      const calculation = this.crossfade / this.totalDuration;
+      return 100 - Math.round(calculation * 100);
+    },
+    htmlString() {
+      return `\n<div class="image-container">\n${this.htmlImages}</div>`;
+    },
+    htmlImages() {
+      let images = '';
+      for (let i = 1; i <= this.totalImages; i += 1) {
+        images += `  <img src="//placehold.it/250x175?text=Image%20${i}">\n`;
+      }
+      return images;
+    },
+    scssString() {
+      return `
+.image-container {
+  position: relative;
+  
+  img {
+    position: absolute;
+    animation-name: multiple-image-crossfade;
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: ${this.totalDuration}s;
+${this.scssDelays}
+  }
+}
+${this.cssKeyframes}
+      `;
+    },
+    scssDelays() {
+      let delays = '';
+      for (let i = 1; i <= this.totalImages; i += 1) {
+        delays += `    &:nth-of-type(${i}) {\n      animation-delay: ${this.calculateDelay(i)}s;\n    }\n`;
+      }
+      return delays;
+    },
+    cssString() {
+      return `
+.image-container {
+  position: relative;
+}
+
+.image-container img {
+  position: absolute;
+  animation-name: multiple-image-crossfade;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+  animation-duration: ${this.totalDuration}s;
+}
+
+${this.cssDelays}
+${this.cssKeyframes}
+      `;
+    },
+    cssDelays() {
+      let delays = '';
+      for (let i = 1; i <= this.totalImages; i += 1) {
+        delays += `.image-container img:nth-of-type(${i}) {\n  animation-delay: ${this.calculateDelay(i)}s;\n}\n\n`;
+      }
+      return delays;
+    },
+    cssKeyframes() {
+      return `
+@keyframes multiple-image-crossfade {
+  0% {
+    opacity:1;
+  }
+  ${this.percentFirst}% {
+    opacity:1;
+  }
+  ${this.percentMiddle}% {
+    opacity:0;
+  }
+  ${this.percentLast}% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}      
+      `;
+    },
+  },
+  watch: {
+    duration: {
+      handler(newValue, oldValue) {
+        if (newValue < 0.1) {
+          this.duration = oldValue;
+        }
+      },
+    },
+    crossfade: {
+      handler(newValue, oldValue) {
+        if (newValue < 0.1) {
+          this.crossfade = oldValue;
+        }
+      },
+    },
+    totalImages: {
+      handler(newValue, oldValue) {
+        if (newValue < 1) {
+          this.totalImages = oldValue;
+        } else {
+          this.totalImages = Math.ceil(newValue);
+        }
+      },
+    },
+  },
+  methods: {
+    calculateDelay(i) {
+      const calculation = this.totalDuration - i * (this.duration + this.crossfade);
+      return Math.round(calculation * 10) / 10;
+    },
   },
 };
 </script>
@@ -21,8 +200,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 60px;
+}
+
+.preview {
+  min-width: 250px;
+  min-height: 175px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue';
+import VueHighlightJS from 'vue-highlight.js';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+import xml from 'highlight.js/lib/languages/xml';
+import 'highlight.js/styles/agate.css';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueHighlightJS, {
+  languages: { css, scss, xml },
+});
+
+Vue.component('v-style', {
+  render(createElement) {
+    return createElement('style', this.$slots.default);
+  },
+});
 
 new Vue({
   render: (h) => h(App),
